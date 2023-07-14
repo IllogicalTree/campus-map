fd514ea7bab70be75bb7c885c06c4202eb2370c5
Jordan Newlands <jordan@dev4.online>
2023-07-14 14:08:27 +0100

    fix colours

--- src/components/IshbelGordon/IshbelGordonLevel7.vue
+++ src/components/IshbelGordon/IshbelGordonLevel7.vue
@@ -1,87 +1,60 @@
 <template>
    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 1122.6667 793.33331">
       <g id="level 7" style="pointer-events: none;">
-         <path class="cls-1"
+         <path class="background"
             d="m 220.07851,375.58541 h 126.38183 v -2.56438 H 516.9819 v -3.25128 h 236.64573 v 4.21292 h 90.64967 v 293.09375 h -90.78383 v 4.12134 H 516.6056 v -4.3503 H 220.14366 Z" />
       </g>
       <g id="AHU 15 RET">
-         <path class="cls-2" d="m 241.27358,523.08096 v 18.58277 h 52.92109 v -18.775 z" />
+         <path class="room" d="m 241.27358,523.08096 v 18.58277 h 52.92109 v -18.775 z" />
       </g>
       <g id="AHU 15 SUP">
-         <path class="cls-2" d="m 241.46131,566.29858 v 18.66821 h 52.33703 v -18.45461 z" />
+         <path class="room" d="m 241.46131,566.29858 v 18.66821 h 52.33703 v -18.45461 z" />
       </g>
       <g id="AHU 12B SUP">
-         <path class="cls-2" d="m 241.25272,589.47365 v 18.43325 h 52.54562 v -18.43325 z" />
+         <path class="room" d="m 241.25272,589.47365 v 18.43325 h 52.54562 v -18.43325 z" />
       </g>
       <g id="AHU 12B RET">
-         <path class="cls-2" d="m 241.39954,626.29244 v 18.60413 h 52.56648 v -18.45461 z" />
+         <path class="room" d="m 241.39954,626.29244 v 18.60413 h 52.56648 v -18.45461 z" />
       </g>
       <g id="AUH 13B RET">
-         <path class="cls-2" d="m 367.17477,617.46948 v 28.79262 h 15.9994 v -28.89941 z" />
+         <path class="room" d="m 367.17477,617.46948 v 28.79262 h 15.9994 v -28.89941 z" />
       </g>
       <g id="AUH 13B SUP">
-         <path class="cls-2"
+         <path class="room"
             d="m 408.04857,595.49055 v 51.13466 h 18.58601 v -51.30553 c 0,0 -18.77375,0.12815 -18.58601,0.17087 z" />
       </g>
       <g id="level 7 stairs 1">
-         <path class="cls-2" d="m 318.42562,395.02143 v 16.0837 h 33.77188 v 5.4894 h 8.03099 v -21.8935 z" />
+         <path class="stair" d="m 318.42562,395.02143 v 16.0837 h 33.77188 v 5.4894 h 8.03099 v -21.8935 z" />
       </g>
       <g id="H702">
-         <path class="cls-2" d="m 341.53818,418.28193 v 9.95353 h 19.46212 v -9.69722 z" />
+         <path class="room" d="m 341.53818,418.28193 v 9.95353 h 19.46212 v -9.69722 z" />
       </g>
       <g id="level 7 DRY RISER 1 ">
-         <path class="cls-2" d="m 341.4756,416.7654 v -2.49906 h 9.24086 v 2.56314" />
+         <path class="room" d="m 341.4756,416.7654 v -2.49906 h 9.24086 v 2.56314" />
       </g>
       <g id="level 7 hot water storage">
-         <path class="cls-2" d="m 362.55582,395.10686 v 23.30322 h 95.18288 v -23.49545 z" />
+         <path class="room" d="m 362.55582,395.10686 v 23.30322 h 95.18288 v -23.49545 z" />
       </g>
       <g id="H701 boiler room">
-         <path class="cls-2" d="m 315.11205,455.80726 h 41.90717 v 66.72709 h -38.08984 v -4.4855 h -3.87991 z" />
+         <path class="room" d="m 315.11205,455.80726 h 41.90717 v 66.72709 h -38.08984 v -4.4855 h -3.87991 z" />
       </g>
       <g id="level 7 electrical riser 1">
-         <path class="cls-2" d="m 311.3573,538.24416 h 4.17194 v 33.91889 h -4.5057 z" />
+         <path class="room" d="m 311.3573,538.24416 h 4.17194 v 33.91889 h -4.5057 z" />
       </g>
       <g id="H704">
-         <path class="cls-2" d="m 347.54891,591.2698 h 13.26677 v 27.72463 h -16.81293 v -7.19815 h 3.79647 z" />
+         <path class="room" d="m 347.54891,591.2698 h 13.26677 v 27.72463 h -16.81293 v -7.19815 h 3.79647 z" />
       </g>
       <g id="level 7 air chillers">
-         <path class="cls-2" d="m 238.46103,455.41267 h 75.57475 v 63.95035 h -11.8066 v -13.52056 h -63.85159 z" />
+         <path class="room" d="m 238.46103,455.41267 h 75.57475 v 63.95035 h -11.8066 v -13.52056 h -63.85159 z" />
       </g>
       <g id="AHU 14B SUP">
-         <path class="cls-2" d="m 241.36053,431.42461 v 18.68956 h 52.7125 v -18.81772 z" />
+         <path class="room" d="m 241.36053,431.42461 v 18.68956 h 52.7125 v -18.81772 z" />
       </g>
       <g id="AHU 14B RET">
-         <path class="cls-2" d="m 241.44397,394.15227 h 52.58734 v 19.07404 h -52.77508 z" />
+         <path class="room" d="m 241.44397,394.15227 h 52.58734 v 19.07404 h -52.77508 z" />
       </g>
       <g id="level 7 stairs 3">
-         <path class="cls-2" d="m 340.68969,621.34937 v -1.3472 h 19.80207 v 26.23616 h -43.37277 v -25.1858 z" />
+         <path class="stair" d="m 340.68969,621.34937 v -1.3472 h 19.80207 v 26.23616 h -43.37277 v -25.1858 z" />
       </g>
    </svg>
 </template>
-
-<style scoped>
-.cls-1,
-.cls-2 {
-   fill: #75378f;
-}
-
-.cls-1 {
-   opacity: 0.55;
-}
-
-.cls-3 {
-   fill: #2d6d9c;
-}
-
-.cls-4 {
-   fill: #4b337f;
-}
-
-.cls-5 {
-   fill: #c26700;
-}
-
-.cls-6 {
-   fill: #b03a4f;
-}
-</style>
