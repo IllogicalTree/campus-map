1512e8c4aef4714ce5c2596a94a245bf9a6f2224
Jordan Newlands <jordan@dev4.online>
2023-07-14 19:02:33 +0100

    fixes and refactors highlight method

--- src/views/SirIanWoodDemo.vue
+++ src/views/SirIanWoodDemo.vue
@@ -1,88 +1,51 @@
 <script setup>
-import { watchEffect, shallowRef } from 'vue';
+import { watchEffect, shallowRef, watch } from 'vue';
 import { useBuildingStore } from '@/stores/building';
-import {useSearchStore } from '@/stores/search';
+import { useSearchStore } from '@/stores/search';
+import { useHighlightStore } from '@/stores/highlight';
 
 const search = useSearchStore();
 const building = useBuildingStore();
-const floorComponent = shallowRef('');
+const highlighted = useHighlightStore();
+const floorComponent = shallowRef();
 
-watchEffect(() => {
-    import(`../components/${building.nameId}/${building.nameId}Level${building.level}.vue`).then(val => floorComponent.value = val.default)
-});
+watchEffect(() => import(`../components/${building.nameId}/${building.nameId}Level${building.level}.vue`).then(val => floorComponent.value = val.default));
 
-let previouslySelectedFill = ''
-let previouslySelectedElement = ''
+const highlight = target => {
+    const element = document.querySelector(`[id='${target?.parentNode?.id}'] > *`);
+    if (element === highlighted.highlightedElement) {
+        highlighted.highlightedElement = null;
+        building.roomId = null;
+        return;
+    };
+    building.roomId = element?.parentNode?.id;
+    highlighted.highlightedElement = element;
+};
 
-const highlight = (target, isSearch) => {
-    if (!target) {
-        return
+watch(() => highlighted.highlightedElement,
+    (highlightedElement, previouslyHighlightedElement) => {
+        highlightedElement?.classList?.add('highlight');
+        previouslyHighlightedElement?.classList?.remove('highlight');
     }
-    building.setRoomId('');
-    if (previouslySelectedElement) {
-        previouslySelectedElement.style.fill = previouslySelectedFill;
-        previouslySelectedElement.style.stroke = ''
-    }
-    if (previouslySelectedElement === target && !isSearch) {
-        previouslySelectedElement = ''
-        previouslySelectedFill = ''
-        return
-    }
-    if (target.style.cssText.includes('hsl(59, 100%, 50%, 0.2)')) {
-        return
-    }
-    building.setRoomId(target?.parentNode?.id);
+);
 
-    previouslySelectedFill = target.style.fill
-    previouslySelectedElement = target
-    target.style.fill = 'hsl(59, 100%, 50%, 0.2)';
-    target.style.stroke = 'yellow';
-}
-
-//highlight(document.querySelector(`[id='${search.query}'] > *`), true);
-
-let previouslySelectedElements = [];
-let previouslySelectedCategory = '';
-
-const highlightAll = category => {
-    
-    if (previouslySelectedCategory === category) {
-        previouslySelectedCategory = '';
-        previouslySelectedElements.forEach(element => {
-            element.style.fill = '';
-            element.style.stroke = '';
-        });
-        previouslySelectedElements = [];
-        return
-    }
-    previouslySelectedCategory = category;
+const highlightCategory = category => {
     const elements = document.querySelectorAll(`[id*='${category.substring(1)}'] `);
-    //const elements = document.querySelectorAll(`[id*='toilet'] `);
-    previouslySelectedElements.forEach(element => {
-        element.style.fill = '';
-        element.style.stroke = '';
-    });
-    previouslySelectedElements = [];
-    elements.forEach(element => {
-        previouslySelectedElements.push(element);
-        element.style.fill = 'hsl(59, 100%, 50%, 0.2)';
-        element.style.stroke = 'yellow';
-    });
-}
+    if (elements === highlighted.highlightedElements) {
+        highlighted.highlightedElements = null;
+        return;
+    };
+    highlighted.highlightedElements = elements;
+};
 
-watchEffect(() => {
-    if (search.query === '') {
-        return
+watch(() => highlighted.highlightedElements,
+    (highlightedElements, previouslyHighlightedElements) => {
+        highlightedElements?.forEach(element => element.classList.add('highlight'));
+        previouslyHighlightedElements?.forEach(element => element.classList.remove('highlight'));
     }
-    highlight(document.querySelector(`[id='${search.query}'] > *`), true);
-})
+);
 
-const onClick = event => {
-    if (!event.target?.parentNode?.id) {
-        return
-    }
-    highlight(event.target);
-}
+watch(() => search.query, () => highlight(document.querySelector(`[id='${search.query}'] > *`)));
 
 </script>
 
@@ -95,19 +58,16 @@ const onClick = event => {
                 <span>{{ building.name }} - Level {{ building.level }}, {{ building.room }}</span>
             </nav>
         </header>
-        <component v-if="floorComponent" :is="floorComponent"
-            @click="(event) => onClick(event)" />
+        <component v-if="floorComponent" :is="floorComponent" @click="event => highlight(event?.target)" />
         <footer>
-            <button @click="highlightAll('accessible_toilet')">Accessible Toilets</button>
-                <button @click="highlightAll('lift')">Lifts</button>
-                <button @click="highlightAll('stair')">Stairs</button>
-                <button @click="highlightAll('bathroom')">Bathrooms</button>
+            <button @click="highlightCategory('accessible_toilet')">Accessible Toilets</button>
+            <button @click="highlightCategory('lift')">Lifts</button>
+            <button @click="highlightCategory('stair')">Stairs</button>
+            <button @click="highlightCategory('bathroom')">Bathrooms</button>
         </footer>
     </main>
 </template>
 
-<script>
-</script>
 <style scoped>
 nav {
     display: flex;
@@ -121,39 +81,43 @@ span {
     margin: .5rem;
 }
 </style>
+
 <style>
 g:hover {
     cursor: pointer;
     stroke: yellow;
 }
-</style>
 
-<style>
 .background,
 .room {
-   fill: #75378f;
+    fill: #75378f;
 }
 
 .background {
-   opacity: 0.55;
+    opacity: 0.55;
 }
 
 .lift {
-   fill: #2d6d9c;
+    fill: #2d6d9c;
 }
 
 .bathroom {
-   fill: #4b337f;
+    fill: #4b337f;
 }
 
 .accessible-toilet {
-   fill: #c26700;
+    fill: #c26700;
 }
 
 .stair {
-   fill: #b03a4f;
+    fill: #b03a4f;
 }
+
 .reception {
     fill: #6c7f28
 }
-</style>
+
+.highlight {
+    fill: hsl(59, 100%, 50%, 0.2);
+    stroke: yellow;
+}</style>
