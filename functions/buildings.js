129db28eeab7d6088129c6446e3a542ce000563f
Jordan <54553879+IllogicalTree@users.noreply.github.com>
2023-07-28 21:41:59 +0100

    adding endpoints

---
+++ functions/buildings.js
@@ -0,0 +1,9 @@
+import { buildings } from '../data';
+
+export const onRequest = () => {
+  return new Response(JSON.stringify(buildings, null, 2), {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  });
+}
