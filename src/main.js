f45813e97b52d48ed06c9042eb07aa18691741cc
Jordan <54553879+IllogicalTree@users.noreply.github.com>
2023-07-28 16:16:24 +0100

    basic pwa functionality

--- src/main.js
+++ src/main.js
@@ -12,3 +12,11 @@ app.use(createPinia())
 app.use(router) //web server
 
 app.mount('#app') //mounts the js onto the div with id 'app' in index.html.. ie the whole app 
+
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onOfflineReady() {
+    console.log('App is offline ready')
+  },
+})
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,31 @@ export default defineConfig({
     svgLoader({
       svgo: false
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'RGU Campus Map',
+        short_name: 'RGU Map',
+        description: 'An interactive map of the Robert Gordon University campus.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
