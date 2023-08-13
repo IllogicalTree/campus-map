5b77641d6e04f53eeb0df81cc99a512df431bd38
Jordan Newlands <illogicaltree@gmail.com>
2023-08-14 00:38:59 +0100

    temp disable importing csv on workers

--- functions/data/buildings.js
+++ functions/data/buildings.js
@@ -1,7 +1,7 @@
-import { buildings } from '../../data';
+//import { buildings } from '../../data';
 
 export const onRequest = () => {
-  return new Response(JSON.stringify(buildings, null, 2), {
+  return new Response(JSON.stringify([], null, 2), {
     headers: {
       "content-type": "application/json;charset=UTF-8",
     },
